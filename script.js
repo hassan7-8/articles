2d187f007e5718f8ccaae80bab79a0f51f52b9c1
hassan7-8 <hassan7534.j@gmail.com>
2026-01-14 11:41:20 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+document.addEventListener("DOMContentLoaded", function() {
+  const form = document.getElementById("contactForm");
+  form.addEventListener("submit", function(e){
+    e.preventDefault();
+    alert("Thank you! Your message has been sent.");
+    form.reset();
+  });
+});
